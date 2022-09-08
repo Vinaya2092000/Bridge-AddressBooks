11bd8c98a47d04c0a77eb1ea40e6b0b011ffadd2
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-09-08 14:35:41 +0530

    [Vinaya]Add: Add Contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -11,15 +11,25 @@
             Address add = new Address();
             while (flag)
             {
-                Console.WriteLine("Enter option: \n 1.Create ");
+                Console.WriteLine("Enter option: \n 1.Create \n 2.Add");
                 Random random = new Random();
                 option = random.Next(0, 2);
                 option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-
                         add.Create();
+                        add.Display();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the number of contacts to add");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 1; i <= n; i++)
+                        {
+                            Console.WriteLine("Enter the details to create contact");
+                            add.Create();
+                        }
+                        Console.WriteLine("These contacts are stored");
                         add.Display();
                         break;
                 }
